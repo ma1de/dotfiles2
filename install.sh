066cbce7abc8f8a940e8433567dd16948b044544
ma1de <ilyabuhtoyarov@protonmail.ch>
2023-11-25 18:47:59 +0500

    d3

--- install.sh
+++ install.sh
@@ -6,3 +6,5 @@ sudo pacman -S neovim neofetch xorg kitty polybar rofi maim xdotool xclip
 git clone https://aur.archlinux.org/paru.git && cd paru && makepkg-si
 
 paru -S shell-color-scripts i3-gaps pfetch
+
+cp -r * ~/.config
