f3ac1c2d39e3149c3a2b132afdf420dc5f3a000a
ma1de <ilyabuhtoyarov@protonmail.ch>
2023-11-25 18:47:10 +0500

    d2

---
+++ install.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+echo "WARNING: This script only works for Arch based distributions!"
+
+sudo pacman -S neovim neofetch xorg kitty polybar rofi maim xdotool xclip
+
+git clone https://aur.archlinux.org/paru.git && cd paru && makepkg-si
+
+paru -S shell-color-scripts i3-gaps pfetch
